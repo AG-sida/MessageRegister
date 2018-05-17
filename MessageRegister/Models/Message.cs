d5be02f0fb35e7340ff5b6899d159e0676d2b4c8
Leonardo Lazcano <leonardoe.lazcano@gmail.com>
2018-05-17 09:58:00 +0200

    Added requirement to MessageUser

--- MessageRegister/Models/Message.cs
+++ MessageRegister/Models/Message.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MessageRegister.Models
 {
     public class Message
     {
         public int Id { get; set; }
-        public DateTime MesaggeTime { get; set; }
+
+           
+        public DateTime MesaggeTime { get; set; } = DateTime.Now;
+
+       
+        [StringLength(160, ErrorMessage = "Your message cannot contain mora than 160 characters")]
         public String MessageUser { get; set; }
     }
 }
+
+
