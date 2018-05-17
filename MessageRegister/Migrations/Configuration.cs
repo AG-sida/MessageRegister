4ee957c029e5f6ba9d14e8fd215c9a44ebbbc80d
Leonardo Lazcano <leonardoe.lazcano@gmail.com>
2018-05-17 09:56:28 +0200

    Change to AutomaticMigration true and no seed objects

--- MessageRegister/Migrations/Configuration.cs
+++ MessageRegister/Migrations/Configuration.cs
@@ -4,23 +4,20 @@ namespace MessageRegister.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    using MessageRegister.Models;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<MessageRegister.DAL.MessageContext>
+    internal sealed class Configuration : DbMigrationsConfiguration <MessageRegister.DAL.MessageContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled =true;
         }
 
         protected override void Seed(MessageRegister.DAL.MessageContext context)
         {
-            context.Messages.AddOrUpdate(x => x.Id,
-          new Message() { Id = 1, MesaggeTime = new DateTime(2012, 06, 15, 13, 45, 00), MessageUser = "Jane Austen" },
-          new Message() { Id = 2, MesaggeTime = new DateTime(2013, 06, 15, 13, 45, 00), MessageUser = "Charles Dickens" },
-          new Message() { Id = 3, MesaggeTime = new DateTime(2017, 06, 15, 13, 45, 00), MessageUser = "Miguel de Cervantes" }
-          );
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
         }
     }
 }
-
